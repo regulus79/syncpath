4f0c279588a6ea09b8c42ae837633ad7043496cc
regulus79 <117475203+regulus79@users.noreply.github.com>
2024-08-16 23:10:57 -0400

    Initial brainstorming for tutorial.lua

---
+++ tutorial.lua
@@ -0,0 +1,23 @@
+--[[
+If this is your first time using syncpath, it is recommended to check out the tutorial by typing '/tutorial'
+
+Welcome to the tutorial! Let's make some awesome music-synced maps!
+To start off, type '/add_keyframe 0' to add a keyframe for bar 0 (the very start) of the music.
+
+Nice, now fly to a different position and type '/add_keyframe 1' to add another keyframe at bar 1.
+Also, pro tip: you can also use '/akf 1' to do the same thing; it's just a little quicker to type.
+
+Great! now test out your path with '/sync'
+
+Not too bad! Now make your path longer by adding a few more keyframes with '/add_keyframe <bar number>' or '/akf <bar number>'
+If you want part of the path to be smooth instead of jagged lines, use '/add_keyframe <bar number> smooth'.
+If you placed a keyframe where you didn't want it to go, you can use '/remove_keyframe <bar number>' to remove it. Or for short, "/rmkf <bar number>"
+
+This path is looking great! Let's get some music going!
+To add music to the sync, type '/music <name of sound file>'. The mod comes with a few default tracks, so let's use those.
+Type '/music real15' to use real15.ogg. If you want to use your own music, put the sound file in the 'sounds' folder in the mod and then restart. Sounds must be converted to .ogg format before minetest can recognize them.
+
+Oh also, we need to set the BPM of the music. 'real15' is 120 bpm, so type '/bpm 120'
+Awesome, now type '/sync' to view your creation!
+
+]]
