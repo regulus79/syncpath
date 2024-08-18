a8d5bbf65033576d0afaad5b2912098c2fd75bdd
regulus79 <117475203+regulus79@users.noreply.github.com>
2024-08-17 21:07:57 -0400

    Rewrite much of tutorial to make it flow better

--- tutorial.lua
+++ tutorial.lua
@@ -31,7 +31,7 @@ local state = 0
 
 local tutorial_message = function(name, message, delay)
     minetest.after(delay or 0, function()
-        minetest.chat_send_player(name, minetest.colorize("#aaffaa", message))
+        minetest.chat_send_player(name, minetest.colorize("#55ff55", message))
     end)
 end
 
@@ -53,27 +53,39 @@ local next_tutorial_state = function(name)
     elseif state == 2 then
         tutorial_message(name, "[tutorial] Great! now test out your path with '/sync'", 1)
     elseif state == 3 then
-        tutorial_message(name, "[tutorial] Not too bad! Now make your path longer by adding a couple more keyframes again with '/add_keyframe <bar>' or '/addkf <bar>'", 3)
+        tutorial_message(name, "[tutorial] Not too bad! Now add 3 more keyframes to make the path longer.", 3)
     elseif state == 4 then
         --tutorial_message(name, "[tutorial] If you want a keyframe to be smooth instead of a jagged corner, use '/add_keyframe <bar number> smooth'.", 13)
-    elseif state == 5 then
-        tutorial_message(name, "[tutorial] This path is looking great! Let's get some music going!", 1)
-        tutorial_message(name, "[tutorial] To add music, type '/music <name of sound file>'. The mod comes with a few default tracks, so let's use those.", 3)
-        tutorial_message(name, "[tutorial] Type '/music real15' to use real15.ogg. If you want to use your own music, put the sound file in the 'sounds' folder in the mod and then restart the game. Sound files must be converted to .ogg format before minetest can recognize them.", 6)
     elseif state == 6 then
-        tutorial_message(name, "[tutorial] Oh also, we need to set the BPM of the music. 'real15.ogg' is 120 bpm, so type '/bpm 120'", 1)
+        tutorial_message(name, "[tutorial] This path is looking great! Let's get some music going!", 1)
+        tutorial_message(name, "[tutorial] To add music, use '/music <name of sound file>'. The mod comes with a few default tracks, so let's use one of them.", 3)
+        tutorial_message(name, "[tutorial] Type '/music real15' to use real15.ogg. If you want to use your own music, put the sound file in the 'sounds' folder in the mod and then restart the game. Sound files must be converted to .ogg format before minetest can recognize them.", 6)
     elseif state == 7 then
-        tutorial_message(name, "[tutorial] Awesome, now type '/sync' to view your creation!", 1)
+        tutorial_message(name, "[tutorial] Oh also, we need to set the BPM of the music. 'real15.ogg' is 120 bpm, so type '/bpm 120'", 1)
     elseif state == 8 then
-        tutorial_message(name, "[tutorial] If you want to hide the path visuals, type '/visuals'", 4)
-        tutorial_message(name, "[tutorial] A few more tips: ", 13)
-        tutorial_message(name, "[tutorial] To make a keyframe smooth, use '/add_keyframe <bar> smooth' or '/addkf <bar> smooth'.", 4)
-        tutorial_message(name, "[tutorial] To make the whole path jagged/smooth, use '/interpolation <linear or smooth>'", 7)
-        tutorial_message(name, "[tutorial] To remove a keyframe, use '/remove_keyframe <bar>'. Or for short, '/rmkf <bar>'", 10)
-        tutorial_message(name, "[tutorial] To replace a keyframe, run '/add_keyframe' again with the same bar number", 16)
-        tutorial_message(name, "[tutorial] To teleport to a certain bar on the path, for example bar 2.5, use '/view_bar 2.5''. ", 20)
-        tutorial_message(name, "[tutorial] You can see a full list of commands by doing '/help'", 23)
-        tutorial_message(name, "[tutorial] You've reached the end of the tutorial!", 28)
+        tutorial_message(name, "[tutorial] Awesome, now type '/sync' to view your creation!", 1)
+    elseif state == 9 then
+        tutorial_message(name, "[tutorial] Now hide the path visuals by doing '/visuals'", 9)
+    elseif state == 10 then
+        tutorial_message(name, "[tutorial] And reshow them with '/visuals' again!", 1)
+    elseif state == 11 then
+        tutorial_message(name, "[tutorial] Just for practice, change the position of the last keyframe a bit. Fly to the position you want it to be, and run '/add_keyframe' again with the same bar number. (So if your last keyframe is bar 4, type '/add_keyframe 4' again)", 1)
+    elseif state == 12 then
+        tutorial_message(name, "[tutorial] And just for practice, remove that last keyframe with '/remove_keyframe <bar>' (short version: '/rmkf <bar>')", 1)
+    elseif state == 13 then
+        tutorial_message(name, "[tutorial] Nice. You know what, let's make the second keyframe be smooth. Replace the keyframe like last time, but with 'smooth' at the end of the command: '/add_keyframe 1 smooth'", 1)
+    elseif state == 14 then
+        tutorial_message(name, "[tutorial] That looks cool! Hmm, actually let's try making the whole path smooth. You can change the interpolation mode for all keyframes by doing '/interpolation smooth'", 1)
+    elseif state == 15 then
+        tutorial_message(name, "[tutorial] Very smooth! Now let's revert it by doing '/interpolation linear' to go back to jagged lines.", 1)
+    elseif state == 16 then
+        tutorial_message(name, "[tutorial] Back to normal! Ok, one last thing. You can use '/view_bar <bar>' to teleport to the part of the track at a certain bar. (short version: '/vb <bar>')", 1)
+        tutorial_message(name, "[tutorial] So if you want to precisely place blocks every quarter bar to sync to your music, then you can do '/vb 0', place a block, '/vb 0.25', place a block '/vb 0.5', place a block, etc...", 8)
+        tutorial_message(name, "[tutorial] There are a few more commands we didn't cover, but you can see a full list using '/help'", 16)
+        tutorial_message(name, "[tutorial] Oh also, don't forget to save your work! Do '/save my_awesome_track', and then when you rejoin do '/load my_awesome_track' to load your project.", 20)
+        tutorial_message(name, "[tutorial] (ALSO you can do '/new' to create a blank project)", 25)
+        tutorial_message(name, "[tutorial] Ok I think that's it for now. So uh, have fun syncing! (is that a word? idk haha)", 30)
+        tutorial_message(name, "[tutorial] WAIT I FORGOT! If the game crashes for some reason, I would appreciate it if you submitted an issue on github. Helps me out. I think. Or maybe it doesn't. Idk. Thanks anyway lol", 37)
         tutorial_active = false
     end
 
@@ -87,14 +99,26 @@ minetest.register_on_chatcommand(function(name, cmd, params)
             if cmd == "add_keyframe" or cmd == "addkf" then next_tutorial_state(name) end
         elseif state == 2 then
             if cmd == "sync" then next_tutorial_state(name) end
-        elseif state == 3 or state == 4 then
+        elseif state == 3 or state == 4 or state == 5 then
             if cmd == "add_keyframe" or cmd == "addkf" then next_tutorial_state(name) end
-        elseif state == 5 then
-            if cmd == "music" then next_tutorial_state(name) end
         elseif state == 6 then
-            if cmd == "bpm" then next_tutorial_state(name) end
+            if cmd == "music" then next_tutorial_state(name) end
         elseif state == 7 then
+            if cmd == "bpm" then next_tutorial_state(name) end
+        elseif state == 8 then
             if cmd == "sync" then next_tutorial_state(name) end
+        elseif state == 9 or state == 10 then
+            if cmd == "visuals" then next_tutorial_state(name) end
+        elseif state == 11 then
+            if cmd == "add_keyframe" or cmd == "addkf" then next_tutorial_state(name) end
+        elseif state == 12 then
+            if cmd == "remove_keyframe" or cmd == "rmkf" then next_tutorial_state(name) end
+        elseif state == 13 then
+            if (cmd == "add_keyframe" or cmd == "addkf") and string.find(params, "smooth") then next_tutorial_state(name) end
+        elseif state == 14 then
+            if cmd == "interpolation" then next_tutorial_state(name) end
+        elseif state == 15 then
+            if cmd == "interpolation" then next_tutorial_state(name) end
         end
     end
 end)
